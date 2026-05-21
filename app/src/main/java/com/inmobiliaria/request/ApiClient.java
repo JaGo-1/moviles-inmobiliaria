00cf2995585dcaa4878c4f1444e86ebb9f5df710
Jacqueline Gomez <137355862+JaGo-1@users.noreply.github.com>
2026-05-21 13:21:55 -0300

    Update ApiClient

--- app/src/main/java/com/inmobiliaria/request/ApiClient.java
+++ app/src/main/java/com/inmobiliaria/request/ApiClient.java
@@ -10,11 +10,13 @@ import com.inmobiliaria.model.Propietario;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public class ApiClient {
 
@@ -47,6 +49,17 @@ public class ApiClient {
         Call<Propietario> obtenerPropietario(
                 @Header("Authorization") String token
         );
+
+        @PUT("api/Propietarios/actualizar")
+        Call<Propietario> actualizarPropietario(@Header("Authorization") String token, @Body Propietario propietario);
+
+        @FormUrlEncoded
+        @PUT("api/Propietarios/changePassword")
+        Call<Void> cambiarClave(
+                @Header("Authorization") String token,
+                @Field("currentPassword") String currentPassword,
+                @Field("newPassword") String newPassword
+        );
     }
 
     public static void guardarToken(Context context, String token) {
@@ -58,7 +71,7 @@ public class ApiClient {
 
         SharedPreferences.Editor editor = sp.edit();
 
-        editor.putString("token", token);
+        editor.putString("token", "Bearer " + token);
 
         editor.apply();
     }
